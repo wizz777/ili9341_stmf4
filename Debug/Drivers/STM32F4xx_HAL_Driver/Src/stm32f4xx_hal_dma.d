6769e3bc3d7aff25828ac2e511e7f2fd5e9923e4
Ivan <wizz@MacBook-Pro-Ivan.local>
2019-09-26 14:02:50 +0300

    upload code

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.d
@@ -0,0 +1,83 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /Users/wizz/Documents/lcd_ili9341/Inc/stm32f4xx_hal_conf.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/cmsis_version.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/mpu_armv7.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sram.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_fsmc.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/Users/wizz/Documents/lcd_ili9341/Inc/stm32f4xx_hal_conf.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/core_cm4.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/cmsis_version.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Include/mpu_armv7.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sram.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_fsmc.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/Users/wizz/Documents/lcd_ili9341/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
